66b8c5331dc79d184d0022413290fa14e8e9b2f4
Gautam-Sarraf <122592561+Gautam-Sarraf@users.noreply.github.com>
2025-03-20 15:53:01 +0530

    no

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/navbar/navbar';
+import Navbar from './navbar/navbar';
 
 function App() {
   return (
